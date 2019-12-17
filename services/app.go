00ad31005b9c6dbe11f22c9d20abd66cb4f6c2d1
Peter Indiola <peter@incube8.sg>
2019-12-17 16:43:40 +0800

    added flag

--- services/app.go
+++ services/app.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -21,6 +22,14 @@ Run ...
 bootstrap
 */
 func Run() {
+
+	limit := flag.Int("limit", 25, "Result limit")
+	sortBy := flag.String("sort", "freq", "Sort by")
+	period := flag.String("period", "14d", "Period")
+	env := flag.String("env", "production", "Environment")
+
+	flag.Parse()
+
 	err := godotenv.Load()
 
 	if err != nil {
@@ -30,7 +39,7 @@ func Run() {
 	sentryURL := os.Getenv("SENTRY_URL")
 	token := os.Getenv("SENTRY_TOKEN")
 
-	sentryURL += "/issues/?query=environment:production+is:unresolved&sort=freq&statsPeriod=14d&limit=25"
+	sentryURL += fmt.Sprintf("/issues/?query=environment:%s+is:unresolved&sort=%s&statsPeriod=%s&limit=%d", *env, *sortBy, *period, *limit)
 	req, error := http.NewRequest("GET", sentryURL, nil)
 
 	req.Header.Add("Authorization", "Bearer "+token)
@@ -65,11 +74,11 @@ func Run() {
 		row := []string{issue.ID, humanize.Comma(i64), humanize.Comma(issue.UserCount), humanize.Time(lastSeen), issue.Title, issue.Permalink}
 
 		if issue.UserCount >= 5000 {
-			table.Rich(row, []tablewriter.Colors{tablewriter.Colors{}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiRedColor}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgWhiteColor}, tablewriter.Colors{tablewriter.Normal, tablewriter.FgHiBlueColor}})
+			table.Rich(row, []tablewriter.Colors{tablewriter.Colors{}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgWhiteColor}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiRedColor}, tablewriter.Colors{tablewriter.Normal, tablewriter.FgHiBlueColor}})
 		} else if issue.UserCount >= 1000 && issue.UserCount <= 5000 {
-			table.Rich(row, []tablewriter.Colors{tablewriter.Colors{}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiYellowColor}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgWhiteColor}, tablewriter.Colors{tablewriter.Normal, tablewriter.FgHiBlueColor}})
+			table.Rich(row, []tablewriter.Colors{tablewriter.Colors{}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgWhiteColor}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiYellowColor}, tablewriter.Colors{tablewriter.Normal, tablewriter.FgHiBlueColor}})
 		} else {
-			table.Rich(row, []tablewriter.Colors{tablewriter.Colors{}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiGreenColor}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgWhiteColor}, tablewriter.Colors{tablewriter.Normal, tablewriter.FgHiBlueColor}})
+			table.Rich(row, []tablewriter.Colors{tablewriter.Colors{}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgWhiteColor}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiGreenColor}, tablewriter.Colors{tablewriter.Normal, tablewriter.FgHiBlueColor}})
 		}
 
 	}
